79683c4e6fb47daeb89b7332991d8b8f6e64fc48
KimHyeockJin <gurwls9628@gmail.com>
2017-12-21 07:58:35 +0900

    Code class 함수 추가 및 OpCode hex코드 정의, global 부분 작성

--- src/PyByteCodeGenVisitor.java
+++ src/PyByteCodeGenVisitor.java
@@ -8,48 +8,118 @@ import Domain.Param.Parameters;
 import Domain.Program;
 import Domain.Stmt.*;
 import Domain.Type_spec.TypeSpecification;
-import com.sun.istack.internal.NotNull;
-
-import javax.xml.bind.DatatypeConverter;
-import java.io.*;
-import java.util.*;
 
 public class PyByteCodeGenVisitor implements ASTVisitor {
-
+    private Code pycCode;
+    private Code currentCode;
+    private int lineNum;
 
     @Override
     public void visitProgram(Program node) {
+        pycCode = new Code();
+        lineNum = 1;
+        if (node.decls == null) {
+            System.out.println("No data! : visitProgram");
+            return ;
+        }
+        for (Declaration declaration : node.decls) {
+            declaration.accept(this);
+        }
+        // TODO : 파이썬으로의 전달 부분
 
+        System.out.println(pycCode.getCode().toString());
     }
 
     @Override
-    public void visitDecl(Declaration node) {
-
-    }
+    public void visitDecl(Declaration node) {}
 
     @Override
     public void visitVar_decl(Variable_Declaration node) {
-
+        if (!pycCode.addNames(node.lhs.getText())) {
+            System.out.println("This value is already exist : visitVar_decl");
+            return ;
+        }
+        // TODO : Global 변수를 0으로 지정해놓을지
     }
 
     @Override
     public void visitVar_decl_array(Variable_Declaration_Array node) {
+        String variableName = node.lhs.getText();
+        if (!pycCode.addNames(variableName)) {
+            System.out.println("This value is already exist : visitVar_decl_array");
+            return ;
+        }
+        int literal = literalNumber(node.rhs.getText());
+        if (literal <= 0) {
+            System.out.println("This array number is le than 0 : visitVar_decl_array");
+            return ;
+        }
+
+        pycCode.addConst(0);
+        for (int i = 0; i < literal; ++i) {
+            pycCode.appendCode(OpCode.LOAD_CONST.getHexCode());
+            pycCode.appendCode(pycCode.indexOfConst(0));
+            pycCode.appendCode(OpCode.STOP_CODE.getHexCode());
+        }
+
+        pycCode.appendCode(OpCode.BUILD_LIST.getHexCode());
+        pycCode.appendCode(String.format("%02x", literal));
+        pycCode.appendCode(OpCode.STOP_CODE.getHexCode());
+
+        pycCode.appendCode(OpCode.STORE_NAME.getHexCode());
+        pycCode.appendCode(pycCode.indexOfNames(variableName));
+        pycCode.appendCode(OpCode.STOP_CODE.getHexCode());
 
     }
 
     @Override
     public void visitVar_decl_assign(Variable_Declaration_Assign node) {
+        String variableName = node.lhs.getText();
+        if (!pycCode.addNames(variableName)) {
+            System.out.println("This value is already exist : visitVar_decl_assign");
+            return ;
+        }
+        int literal = literalNumber(node.rhs.getText());
+
+        pycCode.addConst(literal);
+        pycCode.appendCode(OpCode.LOAD_CONST.getHexCode());
+        pycCode.appendCode(pycCode.indexOfConst(literal));
+        pycCode.appendCode(OpCode.STOP_CODE.getHexCode());
+
+        pycCode.appendCode(OpCode.STORE_NAME.getHexCode());
+        pycCode.appendCode(pycCode.indexOfNames(variableName));
+        pycCode.appendCode(OpCode.STOP_CODE.getHexCode());
 
     }
 
     @Override
-    public void visitType_spec(TypeSpecification node) {
-
-    }
+    public void visitType_spec(TypeSpecification node) {}
 
     @Override
     public void visitFun_decl(Function_Declaration node) {
+        String functionName = node.t_node.getText();
+        if (!pycCode.addNames(functionName)) {
+            System.out.println("Function name is already exist : visitFun_decl");
+            return ;
+        }
+        currentCode = new Code();
 
+        node.params.accept(this);
+        node.compount_stmt.accept(this);
+
+        pycCode.addConst(currentCode);
+
+        pycCode.appendCode(OpCode.LOAD_CONST.getHexCode());
+        pycCode.appendCode(pycCode.indexOfConst(currentCode));
+        pycCode.appendCode(OpCode.STOP_CODE.getHexCode());
+
+        pycCode.appendCode(OpCode.MAKE_FUNCTION.getHexCode());
+        pycCode.appendCode(OpCode.STOP_CODE.getHexCode());
+        pycCode.appendCode(OpCode.STOP_CODE.getHexCode());
+
+        pycCode.appendCode(OpCode.STORE_NAME.getHexCode());
+        pycCode.appendCode(pycCode.indexOfNames(functionName));
+        pycCode.appendCode(OpCode.STOP_CODE.getHexCode());
     }
 
     @Override
@@ -161,4 +231,22 @@ public class PyByteCodeGenVisitor implements ASTVisitor {
     public void visitArgs(Arguments node) {
 
     }
+
+
+
+    private int literalNumber(String number) {
+        if (number.length() > 2) {
+            if (number.charAt(1) == 'x' || number.charAt(1) == 'X') {
+                return Integer.parseInt(number.substring(2, number.length()), 16);
+            }
+        }
+        if (number.length() > 1) {
+            if (number.charAt(0) == '0') {
+                return Integer.parseInt(number, 8);
+            }
+        }
+        return Integer.parseInt(number);
+    }
+
+
 }
